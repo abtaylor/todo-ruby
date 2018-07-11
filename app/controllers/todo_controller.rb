7541507616ba12317f8568d18756639862322c50
Alyssa Taylor <alyssa.taylor@hlcsk12.net>
2018-07-11 14:22:47 +0000

    working instance variable

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,8 @@
 class TodoController < ApplicationController 
     def index
     end
+    def show
+        @todo_description = "English Homework"
+        @todo_pomodoro_estimate = 2
+    end
 end
